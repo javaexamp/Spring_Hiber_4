83761c069be7027a187510c954cd7f3365abbd34
javaexamp <javaexamp@mailbox.com>
2023-04-19 18:10:45 +0300

    First commit

---
+++ src/main/java/hiber/Main.java
@@ -0,0 +1,34 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class Main {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      userService.add(new User("User1", "Lastname1", "user1@yandex.ru"));
+      userService.add(new User("User2", "Lastname2", "user2@yandex.ru"));
+      userService.add(new User("User3", "Lastname3", "user3@yandex.ru"));
+      userService.add(new User("User4", "Lastname4", "user4@yandex.ru"));
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println();
+      }
+
+      context.close();
+   }
+}
